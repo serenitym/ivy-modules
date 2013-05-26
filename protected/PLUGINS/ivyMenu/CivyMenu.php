ec813b36ebf91b4d249ef1d98277af1b485a221a
Ioana <ioana@serenitymedia.ro>
2013-05-26 08:15:47 +0300

    26 may 2013

--- protected/PLUGINS/ivyMenu/CivyMenu.php
+++ protected/PLUGINS/ivyMenu/CivyMenu.php
@@ -50,7 +50,7 @@ class CivyMenu{
             foreach($ch as $idCh){
 
                 $idC = $idCh;
-                $name = $this->masterTree[$idC]->name;
+                $name = $this->masterTree[$idC]->{'name_'.$this->lang};
                 $href = publicURL . "?idT={$idT}&idC={$idC}";
 
                 array_push($item['items'],
@@ -114,7 +114,7 @@ class CivyMenu{
           #   var_dump($this->masterTree);
               while($row  = $res->fetch_assoc())
               {
-                      $name = $row['name_'.$this->LG];
+                      $name = $row['name_'.$this->lang];
                       $idC   = $row['id'];
                       $idT   = $this->masterTree[$idC]->idT;
 
@@ -206,6 +206,7 @@ class CivyMenu{
      *
     */
     function _setRes($resPath){
+
 
         $currentSet = &$this->{'menuSet'.$this->current_idMenu};
         $currentSet['strUl'] = '';
