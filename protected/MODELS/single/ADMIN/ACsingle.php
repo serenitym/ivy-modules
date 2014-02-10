0e34e7cecb1247957ecb1ee908af3a274f8b3963
Victor Nițu <victor@debian.org.ro>
2014-02-10 16:40:25 +0200

    Hardcoded single model

--- protected/MODELS/single/ADMIN/ACsingle.php
+++ protected/MODELS/single/ADMIN/ACsingle.php
@@ -3,7 +3,7 @@ class ACsingle extends Csingle
 {
     var $post;
 
-    function _hook_savePage()
+    function savePage()
     {
        /*if($this->contentRights) {
            $this->C->feedBack->Set_mess(
@@ -19,19 +19,21 @@ class ACsingle extends Csingle
                                $this->post->resName);
        $this->post->desc = $_POST['desc'];
 
-       //error_log("********* [ ivy ] _hook_save_descPage");
        //var_dump($this->post);
-       //return false;
-       return true;
-    }
-    function savePage()
-    {
-      // var_dump($this->post);
+
        Toolbox::Fs_writeTo($this->post->pathName, $this->post->desc);
        //file_put_contents($this->post->pathName, $this->post->desc);
 
        //return false;
        return true;
     }
+
+    public function _init_()
+    {
+        parent::_init_();
+
+        if (isset($_REQUEST['save_single'])) {
+            $this->savePage();
+        }
 
 }
